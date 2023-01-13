2d553ff38468171f6ef8c93a40ae4c1e2d6c633a
Nick Abbott <nicholasabbott98@gmail.com>
2023-01-13 15:12:32 -0600

    Time: 62 ms (71.75%), Space: 13.2 MB (89.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,9 +4,12 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
+        # Turn number into string
         num = str(x)
+        # Save a reversed version of the string
         reversed_num = num[::-1]
         
+        # If reversed = original return True since that is a palindrome
         if num == reversed_num:
             return True
         
