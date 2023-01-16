ba4f098a579b674d944550c6bee1311d69400bb3
Nick Abbott <nicholasabbott98@gmail.com>
2023-01-16 14:34:11 -0600

    Time: 40 ms, Memory: 14.1 MB - LeetHub

---
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def even(n):
+            count = 0
+            while n != 0:
+                n //= 10
+                count += 1
+            
+            if count % 2 == 0:
+                return 1
+            else:
+                return 0
+            
+            print(map(even, nums))
+        return sum(map(even, nums))
