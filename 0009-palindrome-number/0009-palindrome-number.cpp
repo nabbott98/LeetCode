6a9f15e72cc841b5fa5c755c3917a77f7b19bc1e
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-15 11:15:07 -0500

    Time: 36 ms (9.52%), Space: 6 MB (62.47%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,11 +1,8 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-  if (x < 0) return false
-
-  let rev = 0
-  for(let i = x; i >= 1; i = Math.floor(i/10)) rev = rev*10 + i%10
-  return rev === x
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string rev = to_string(x);
+        reverse(rev.begin(), rev.end());
+        return to_string(x) == rev;
+    }
 };
