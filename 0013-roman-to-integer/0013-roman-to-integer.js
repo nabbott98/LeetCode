ea95022f2845de563073cdc2f046fc68649feb9f
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-15 11:19:49 -0500

    Time: 131 ms (63.91%), Space: 45.9 MB (94.84%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -1,22 +1,15 @@
-class Solution {
-public:
-    int romanToInt(string s) {
-        unordered_map<char,int> mp{
-            {'I',1},
-            {'V',5},
-            {'X',10},
-            {'L',50},
-            {'C',100},
-            {'D',500},
-            {'M',1000},
-        };
-        int ans =0;
-        for(int i=0;i<s.size();i++){
-            if(mp[s[i]]<mp[s[i+1]])
-                ans-=mp[s[i]];
-            else
-                ans+=mp[s[i]];
-        }
-        return ans;
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const map = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+    let num = 0;
+    
+    for(let i = 0; i < s.length; i++) {
+        const curr = map[s[i]], next = map[s[i+1]];
+        if(curr < next) num -= curr;
+        else num += curr;
     }
+    return num;
 };
