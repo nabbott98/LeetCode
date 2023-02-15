cadfb358b3608378f3eefb6029d37997fc05b299
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-15 11:16:58 -0500

    Time: 16 ms (54.25%), Space: 6.1 MB (9.28%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -1,5 +1,17 @@
-function isPalindrome(x: number): boolean {
-    var rev = 0, xCopy = x; // creating variables for reversed integer and a copy for x
-    while (xCopy > 0) { rev = rev * 10 + xCopy % 10; xCopy = Math.floor(xCopy/10); } // I did xCopy > 0 instead of xCopy != 0 so that it excludes negative integers
-    return (rev == x && xCopy > -1); // Returns true only when rev is equal to x AND xCopy isn't negative
-};
+bool isPalindrome(int x){
+    char x_s[12];
+    sprintf(x_s, "%d", x);
+
+    int left = 0;
+    int right = strlen(x_s)-1;
+
+    while (left < right) {
+
+        if (x_s[left] != x_s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
