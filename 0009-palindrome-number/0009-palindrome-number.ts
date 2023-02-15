bebec97e0b9f3dbf7a292e6b898475387eb2dc31
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-15 11:16:03 -0500

    Time: 196 ms (40.54%), Space: 51.2 MB (83.50%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.ts
+++ 0009-palindrome-number/0009-palindrome-number.ts
@@ -1,8 +1,5 @@
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        string rev = to_string(x);
-        reverse(rev.begin(), rev.end());
-        return to_string(x) == rev;
-    }
+function isPalindrome(x: number): boolean {
+    var rev = 0, xCopy = x; // creating variables for reversed integer and a copy for x
+    while (xCopy > 0) { rev = rev * 10 + xCopy % 10; xCopy = Math.floor(xCopy/10); } // I did xCopy > 0 instead of xCopy != 0 so that it excludes negative integers
+    return (rev == x && xCopy > -1); // Returns true only when rev is equal to x AND xCopy isn't negative
 };
