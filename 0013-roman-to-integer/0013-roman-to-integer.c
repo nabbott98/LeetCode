aca05604d3cf8fde7b0537cfaa950358a5cc1017
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-15 11:21:10 -0500

    Time: 8 ms (66.11%), Space: 5.8 MB (58.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.c
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -1,15 +1,19 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    const map = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
-    let num = 0;
-    
-    for(let i = 0; i < s.length; i++) {
-        const curr = map[s[i]], next = map[s[i+1]];
-        if(curr < next) num -= curr;
-        else num += curr;
+int romanToInt(char * s){
+    int t['X' + 1] = {
+        ['I'] = 1,
+        ['V'] = 5,
+        ['X'] = 10,
+        ['L'] = 50,
+        ['C'] = 100,
+        ['D'] = 500,
+        ['M'] = 1000,
+    };
+    int res = 0;
+    for (int i = 0; s[i]; i++) {
+        if (t[s[i]] < t[s[i+1]])
+            res -= t[s[i]];
+        else
+            res += t[s[i]];
     }
-    return num;
-};
+    return res;
+}
