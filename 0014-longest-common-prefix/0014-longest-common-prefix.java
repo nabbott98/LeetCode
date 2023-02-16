299ea31159f7f0ba91e71fdd98d653a7104ee25f
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-16 13:33:40 -0500

    Time: 0 ms (100.00%), Space: 40.1 MB (77.71%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length < 1 || strs == null) {
+            return "";
+        }
+        if (strs.length == 1) {
+            return strs[0];
+        }
+        //find the shortest String
+        int shortest = 0;
+        int len = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            int curLen = strs[i].length();
+            if (curLen < len) {
+                len = curLen;
+                shortest = i;
+            }
+        }
+        //find the longest common prefix
+        String sub = strs[shortest];
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(sub) != 0) {
+                sub = sub.substring(0, sub.length()-1);
+            }
+        } 
+        return sub;
+    }
+}
