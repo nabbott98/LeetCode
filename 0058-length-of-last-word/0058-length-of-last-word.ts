82908107d4ece83cd9a142d4cf273ffd2824863f
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-17 11:34:41 -0500

    Time: 57 ms (95.84%), Space: 43.7 MB (8.43%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.ts
+++ 0058-length-of-last-word/0058-length-of-last-word.ts
@@ -1,21 +1,6 @@
-int lengthOfLastWord(char * s){
-    int c=0;
-    for(int i=strlen(s)-1;i>=0;i--){
-        if(s[i]==' '){
-            c++;
-        }
-        else{
-            break;
-        }
-    }
-    int c1=0;
-    for(int i=strlen(s)-c-1;i>=0;i--){
-        if(s[i]!=' '){
-            c1++;
-        }
-        else{
-            break;
-        }
-    }
-    return c1;
-}
+function lengthOfLastWord(s: string): number {
+  const words = s.split(" ").filter((v) => v)
+  const maybeLast = words[words.length - 1]
+
+  return maybeLast ? maybeLast.length : 0
+};
