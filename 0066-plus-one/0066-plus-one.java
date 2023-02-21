6488e5ac2378363fb03543b25b5d0b473c7a4027
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-21 11:48:00 -0500

    Time: 0 ms (100.00%), Space: 42.6 MB (7.32%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,11 +1,15 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        num = 0
-        for i in range(len(digits)):
-            num += digits[i] * pow(10, (len(digits)-1-i))
-        return [int(i) for i in str(num+1)]
-        
+class Solution {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
