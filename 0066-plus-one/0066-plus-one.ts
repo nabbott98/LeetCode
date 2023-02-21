d4658afdc4e58014acad87592d49854f5360d2a8
Nick Abbott <nicholasabbott98@gmail.com>
2023-02-21 11:48:51 -0500

    Time: 92 ms (10.25%), Space: 44.3 MB (6.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,15 +1,3 @@
-class Solution {
-    public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits;
-            }
-            digits[i] = 0;
-        }
-
-        digits = new int[digits.length + 1];
-        digits[0] = 1;
-        return digits;
-    }
-}
+function plusOne(digits: number[]): number[] {
+    return Array.from((BigInt(digits.join('')) + BigInt('1')).toString(), Number)
+};
