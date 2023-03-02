4594b6ffb9565430a58eb27694a2501b803a208a
Nick Abbott <nicholasabbott98@gmail.com>
2023-03-02 07:57:40 -0700

    Time: 29 ms (54.34%), Space: 15.9 MB (78.30%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -1,28 +1,12 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
-function maxDepth(root: TreeNode | null): number {
-    if(!root) return 0;
-    
-    return DFS(root, 0)
-}; 
-
-function DFS(root: TreeNode | null, count : number): number {
-    if(!root){
-        return count;
-    }
-    
-    count++;
-    return Math.max(DFS(root.left, count), DFS(root.right, count))
-}
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        
